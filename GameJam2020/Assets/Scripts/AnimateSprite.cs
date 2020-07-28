f6670f98b009b8dafdb8b2ae585ea0efd7f41b4e
Michaela <michaelacfaith@gmail.com>
2020-07-27 18:09:11 -0700

    Title animation done

---
+++ GameJam2020/Assets/Scripts/AnimateSprite.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateSprite : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private float timeBetweenSprites;
+    private SpriteRenderer m_spriteRend;
+
+    private void Awake()
+    {
+        m_spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(Animate());
+    }
+
+    private void Update()
+    {
+
+    }
+
+    IEnumerator Animate()
+    {
+        while (true)
+        {
+            foreach (Sprite sprite in sprites)
+            {
+                m_spriteRend.sprite = sprite;
+                yield return new WaitForSeconds(timeBetweenSprites);
+            }
+        }
+        
+    }
+}
