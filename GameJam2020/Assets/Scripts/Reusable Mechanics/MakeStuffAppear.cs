99aa2d5b1cddc1c1aa6cfdba1c0ea599beb25b1a
Michaela <michaelacfaith@gmail.com>
2020-07-27 19:41:52 -0700

    Panel1 Ch2 done

---
+++ GameJam2020/Assets/Scripts/Reusable Mechanics/MakeStuffAppear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeStuffAppear : MonoBehaviour
+{
+    [SerializeField] GameObject[] stuff;
+    private int i = 0;
+
+    private void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            // If all objects are present
+            if (i == (stuff.Length))
+            {
+                print("Next Scene!");
+            }
+            else
+            {
+                stuff[i].SetActive(true);
+                i++;
+            }
+        }
+    }
+
+}
