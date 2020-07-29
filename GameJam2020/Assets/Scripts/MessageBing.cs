b2473f6aada01a2c38a7437fd172711db3f874e1
Michaela <michaelacfaith@gmail.com>
2020-07-28 20:25:57 -0700

    ch3pan4 done

---
+++ GameJam2020/Assets/Scripts/MessageBing.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageBing : MonoBehaviour
+{
+    [SerializeField] private Sprite newMSG;
+    [SerializeField] private Sprite newMSGhover;
+    [SerializeField] private float waitTime;
+
+    [SerializeField] private GameObject chat;
+    [SerializeField] private float chatY;
+
+    private SpriteRenderer m_sprite;
+
+    void Start()
+    {
+        m_sprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(WaitThenPing());
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator WaitThenPing()
+    {
+        yield return new WaitForSeconds(waitTime);
+
+        //sound sfx
+        m_sprite.sprite = newMSG;
+    }
+
+    private void OnMouseOver()
+    {
+        // Pinged
+        if (m_sprite.sprite == newMSG)
+        {
+            m_sprite.sprite = newMSGhover;
+        }
+
+        // Open Message
+        if (Input.GetMouseButtonDown(0))
+        {
+            chat.SetActive(true);
+            Vector3 newPos = chat.transform.position;
+            newPos.y = chatY;
+            chat.transform.position = Vector3.Lerp(chat.transform.position, newPos, Time.time);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (m_sprite.sprite == newMSGhover)
+        {
+            m_sprite.sprite = newMSG;
+        }
+    }
+
+    private IEnumerator SmoothMove(Vector3 currentPos, Vector3 targetPos, float waitTime)
+    {
+
+        while (chat.transform.position != targetPos)
+        {
+            chat.transform.position = Vector3.Lerp(currentPos, targetPos, 0.1f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+}
