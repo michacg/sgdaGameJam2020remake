d2e5d3251d1534ffb90d330ed467fd0ed92454c4
Michaela <michaelacfaith@gmail.com>
2020-07-29 23:03:42 -0700

    pan9 int

---
+++ GameJam2020/Assets/Scripts/ClickToControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToControl : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 0.1f;
+    [SerializeField] private GameObject onTrack;
+    [SerializeField] private GameObject offTrack;
+
+    private Vector3 mousePosition;
+
+    bool OnPanel = false;
+
+    private void Awake()
+    {
+        GetComponentInParent<Panel>().OnPanel += Activate;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && OnPanel)
+        {
+            mousePosition=Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("target") && onTrack.activeSelf)
+        {
+            OnPanel = false;
+            ChapterManager.instance.NextPanel();
+        }
+
+        onTrack.SetActive(true);
+        offTrack.SetActive(false);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onTrack.SetActive(false);
+        offTrack.SetActive(true);
+    }
+
+    void Activate()
+    {
+        OnPanel = true;
+    }
+}
