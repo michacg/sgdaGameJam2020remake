d9364483836166e71dfb4fd8ff798687d67f520d
Michaela <michaelacfaith@gmail.com>
2020-07-29 17:13:37 -0700

    Pan6 integrated

---
+++ GameJam2020/Assets/Scripts/MoveWing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWing : MonoBehaviour
+{
+    [SerializeField] private GameObject xeth;
+    [SerializeField] private GameObject newXeth;
+    [SerializeField] private float rotZ;
+
+    bool OnPanel = false;
+
+    private void Awake()
+    {
+        GetComponentInParent<Panel>().OnPanel += Activate;
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && OnPanel) //And panel on
+        {
+            if (newXeth.activeSelf)
+            {
+                ChapterManager.instance.NextPanel();
+                OnPanel = false;
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, rotZ));
+                xeth.SetActive(false);
+                newXeth.SetActive(true);
+
+            }
+        }
+    }
+
+    void Activate()
+    {
+        OnPanel = true;
+    }
+}
