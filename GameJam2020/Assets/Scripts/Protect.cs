cdb0545e8e5ca12467413adfde291ce45a0262d6
Michaela <michaelacfaith@gmail.com>
2020-07-29 18:02:49 -0700

    pan7 integrated

---
+++ GameJam2020/Assets/Scripts/Protect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protect : MonoBehaviour
+{
+    [SerializeField] private float moveInc;
+    [SerializeField] private GameObject wingPivot;
+    [SerializeField] private float rotZ;
+
+    bool OnPanel = false;
+
+    private void Awake()
+    {
+        GetComponentInParent<Panel>().OnPanel += Activate;
+    }
+
+    void Update()
+    {
+        if (OnPanel && (Input.GetMouseButton(0) || Input.GetAxis("Horizontal") > 0)) //on panel
+        {
+            Vector3 newPos = transform.position;
+            newPos.x += moveInc;
+
+            if (wingPivot.transform.rotation != Quaternion.Euler(new Vector3(0, 0, rotZ)))
+                transform.position = newPos;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Protected SFX!
+        wingPivot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, rotZ));
+
+        ChapterManager.instance.NextPanel();
+        OnPanel = false;
+
+    }
+
+    void Activate()
+    {
+        OnPanel = true;
+    }
+}
