6bc641b785fbba4ddc1b6d25b53ab1c374ca9896
Michaela <michaelacfaith@gmail.com>
2020-07-31 16:03:25 -0700

    working on BGM for ch3

---
+++ GameJam2020/Assets/BGM.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public static BGM instance;
+
+    [SerializeField] private AudioClip[] songs;
+    [SerializeField] private AudioSource m_audio;
+    private int track = 0;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        m_audio = GetComponent<AudioSource>();    
+    }
+
+    // Update is called once per frame
+    private void Start()
+    {
+        m_audio.Play();
+    }
+
+    public void ChangeSong()
+    {
+        m_audio.clip = songs[track];
+        track++;
+        m_audio.Play();
+    }
+}
--- GameJam2020/Assets/Scripts/Panel.cs
+++ GameJam2020/Assets/Scripts/Panel.cs
@@ -7,12 +7,19 @@ public class Panel: MonoBehaviour
     public delegate void Focused();
     public Focused OnPanel;
 
+    public bool changeBGM = false;
+
 
     public virtual void Activate()
     {
         if(OnPanel != null)
         {
             OnPanel.Invoke();
+        }
+
+        if (changeBGM)
+        {
+            BGM.instance.ChangeSong();
         }
     }
 }
