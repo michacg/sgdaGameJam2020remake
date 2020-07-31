47f80edfffb7ab770d8b676c480b9eba22992e12
Michaela <michaelacfaith@gmail.com>
2020-07-30 21:34:01 -0700

    ch2 pan 2 thru 4 in

---
+++ GameJam2020/Assets/LevelUp.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUp : MonoBehaviour
+{
+    //ya ummm sorry this code is straight up ugly lolm y b<3
+    [SerializeField] private SpriteRenderer m_sprite;
+    [SerializeField] private Sprite buttonUp;
+    [SerializeField] private Sprite buttonDown;
+    [SerializeField] private GameObject ageObj;
+    [SerializeField] private Sprite[] numSprites;
+    [SerializeField] private GameObject pic;
+    [SerializeField] private Sprite[] ageSprites;
+
+    private int age = 0;
+
+    [SerializeField] private int perClick = 5;
+
+    [SerializeField]  private int clicks = 0;
+    bool OnPanel = false;
+    
+    void Awake()
+    {
+        GetComponentInParent<Panel>().OnPanel += Activate;
+        m_sprite = GetComponent<SpriteRenderer>();
+        m_sprite.sprite = buttonUp;
+    }
+
+    void Update()
+    {
+        print(age);
+        if (age == numSprites.Length)
+        {
+            ChapterManager.instance.NextPanel();
+            OnPanel = false;
+        }
+
+        if (age + 7 == 13)
+            pic.GetComponent<SpriteRenderer>().sprite = ageSprites[0];
+
+        if (age + 7 == 18)
+            pic.GetComponent<SpriteRenderer>().sprite = ageSprites[1];
+       
+    }
+
+    private void OnMouseDown()
+    {
+        m_sprite.sprite = buttonDown;
+        clicks++;
+
+
+        // multiples of 5
+        if (clicks % 5 == 0)
+        {
+            ageObj.GetComponent<SpriteRenderer>().sprite = numSprites[++age];
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        m_sprite.sprite = buttonUp;
+    }
+
+    void Activate()
+    {
+        OnPanel = true;
+    }
+}
