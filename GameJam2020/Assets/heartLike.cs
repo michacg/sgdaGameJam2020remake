ca2d254f80bfc9d1655b1e3be55130ec1cf70c56
Michaela <michaelacfaith@gmail.com>
2020-07-31 14:05:25 -0700

    CH2 done

---
+++ GameJam2020/Assets/heartLike.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heartLike : MonoBehaviour
+{
+    [SerializeField] private GameObject xeth;
+    [SerializeField] private Sprite xethPhone;
+    private SpriteRenderer m_sprite;
+    private bool clicked = false;
+
+    private void Awake()
+    {
+        m_sprite = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void OnMouseDown()
+    {
+        m_sprite.color = new Color(1, 1, 1, 1f);
+        clicked = true;
+    }
+    private void OnMouseOver()
+    {
+        if (!clicked)
+            m_sprite.color = new Color(1, 1, 1, 0.5f);
+        xeth.GetComponent<SpriteRenderer>().sprite = xethPhone;
+    }
+
+    private void OnMouseExit()
+    {
+        if (!clicked)
+        {
+            m_sprite.color = new Color(1, 1, 1, 0f);
+        }
+
+    }
+}
---
+++ GameJam2020/Assets/phone.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class phone : MonoBehaviour
+{
+
+    [SerializeField] private GameObject xeth;
+    [SerializeField] private Sprite xethLay;
+    [SerializeField] private Sprite xethPhone;
+    [SerializeField] private GameObject heart0;
+    [SerializeField] private GameObject heart1;
+
+    [SerializeField] private float yPos;
+
+    bool OnPanel = false;
+
+    void Awake()
+    {
+        GetComponentInParent<Panel>().OnPanel += Activate;
+    }
+
+        void Update()
+    {
+        if (heart0.GetComponent<SpriteRenderer>().color == new Color(1,1,1f))
+        {
+            Vector3 newPos = transform.position;
+            newPos.y = yPos;
+            transform.position = Vector2.Lerp(transform.position, newPos, 0.1f);
+        }
+        if (heart1.GetComponent<SpriteRenderer>().color == new Color(1, 1, 1f))
+        {
+            //DONE
+            ChapterManager.instance.NextPanel();
+            OnPanel = false;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        xeth.GetComponent<SpriteRenderer>().sprite = xethPhone;
+    }
+
+    private void OnMouseExit()
+    {
+        xeth.GetComponent<SpriteRenderer>().sprite = xethLay;
+    }
+
+
+    void Activate()
+    {
+        OnPanel = true;
+    }
+
+}
