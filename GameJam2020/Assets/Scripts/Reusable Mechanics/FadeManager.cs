c9d706fae7343a93665df4ce8a6a836ffd98693a
Michelle Wang <zhiyimichelle@gmail.com>
2020-07-31 18:51:30 -0700

    fixed fade manager, can translate to 2+ chapters >:)

--- GameJam2020/Assets/Scripts/Reusable Mechanics/FadeManager.cs
+++ GameJam2020/Assets/Scripts/Reusable Mechanics/FadeManager.cs
@@ -5,22 +5,20 @@ using UnityEngine.SceneManagement;
 //manage fading in/out of scenes
 public class FadeManager : MonoBehaviour
 {
-    public FadeManager instance = null;
     private Animator animator;
     [SerializeField] private  AnimationClip FadeIn;
 
     void Awake(){
          animator = GetComponent<Animator>();
-        if(instance != null)
-        {
-            Destroy(instance.gameObject);
-        }
-        instance = this;
     }
     // Start is called before the first frame update
     void Start()
     {
        animator.Play("FadeOUT"); 
+        Debug.Log("fade manager events: " +  FadeIn.events.Length);
+        if(FadeIn.events.Length > 0){
+            Debug.Log(FadeIn.events[0].stringParameter);
+        }
     }
 
     // Update is called once per frame
@@ -29,14 +27,17 @@ public class FadeManager : MonoBehaviour
         
     }
     public void FadeSceneOut(string scenename){
+       if(FadeIn.events.Length == 0){
         Debug.Log("fading to: " + scenename);
-        AnimationEvent evt = new AnimationEvent();
-        evt.stringParameter = scenename;
-        evt.time = 0.57f;
-        evt.functionName = "LoadScene";
-        FadeIn.AddEvent(evt);
-        
+            AnimationEvent evt = CreateSceneTransitionEvent(scenename);
+            FadeIn.AddEvent(evt);
+       }else{
+            AnimationEvent[] animationEvents = new AnimationEvent[]{CreateSceneTransitionEvent(scenename)};
+            //FadeIn.events[0].stringParameter=scenename;
+            FadeIn.events = animationEvents;
+       }
         animator.Play("FadeIN");
+       
     }
     public void LoadScene(string scenename){
         SceneManager.LoadScene(scenename);
@@ -47,5 +48,13 @@ public class FadeManager : MonoBehaviour
     
     public void SetActive(){
         this.gameObject.SetActive(true);
+    }
+    private AnimationEvent CreateSceneTransitionEvent(string scenename){
+        AnimationEvent evt = new AnimationEvent();
+        evt.stringParameter = scenename;
+        evt.time = 0.57f;
+        evt.functionName = "LoadScene";
+        return evt;
+        
     }
 }
