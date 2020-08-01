ffc36dbbdd10e267d765ac112e74391e358755d5
Michaela <michaelacfaith@gmail.com>
2020-08-01 12:53:04 -0700

    eh

---
+++ GameJam2020/Assets/FakeUI2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeUI2 : MonoBehaviour
+{
+    [SerializeField] private GameObject fakeUI;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = fakeUI.GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseOver()
+    {
+        sprite.color = new Color(1, 1, 1, 0.5f);
+    }
+
+    private void OnMouseExit()
+    {
+        sprite.color = new Color(1, 1, 1, 1);
+    }
+}
